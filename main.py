c79fb5b8cfd226c41424c1742ec664c05d6d3d11
sadeceee <tim-kilian@hotmail.de>
2015-01-24 00:08:13 +0100

    create window

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.set_caption("BoomB0B")
+
+    clock = pygame.time.Clock()
+    done = False
+
+    while not done:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        screen.fill(GREEN)
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()

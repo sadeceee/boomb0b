c9a5279ee2f160717aea31ae6b27330450c52edd
Pascal Ehlke <pe@capricorn.de>
2015-01-24 07:33:05 +0100

    Added lists for every field of the gamefield

--- gamefield.py
+++ gamefield.py
@@ -1,5 +1,3 @@
-from constants import *
-from box import *
 from player import *
 import random
 
@@ -13,19 +11,25 @@ class gamefield:
         for y in range(FIELDS_Y):
             for x in range(FIELDS_X):
                 if(self.fields[y][x] == '1'):
-                    self.fields[y][x] = stone()
+                    self.fields[y][x] = ()
+                    self.fields[y][x].append(stone())
                 elif(self.fields[y][x] == '2'):
                     iRandom = random.randint(2, 30)
                     if (iRandom % 2 == 0):
-                        self.fields[y][x] = crate()
+                        self.fields[y][x] = ()
+                        self.fields[y][x].append(crate())
                     else:
-                        self.fields[y][x] = boden()
+                        self.fields[y][x] = ()
+                        self.fields[y][x].append(boden())
                 elif(self.fields[y][x] == '3'):
-                    self.fields[y][x] = player_1()
+                    self.fields[y][x] = ()
+                    self.fields[y][x].append(player_1())
                 elif(self.fields[y][x] == '4'):
-                    self.fields[y][x] = bomb(self, 3, x, y)
+                    self.fields[y][x] = ()
+                    self.fields[y][x].append(bomb(self, 3, x, y))
                 else:
-                    self.fields[y][x] = boden()
+                    self.fields[y][x] = ()
+                    self.fields[y][x].append(boden());
 
         self.c_screen = screen
 
@@ -38,11 +42,15 @@ class gamefield:
         for y in range(FIELDS_Y):
             for x in range(FIELDS_X):
                 if (self.fields[y][x].update(self, x, y) == True):
-                    self.fields[y][x] = boden()
+                    self.rem(self.fields[y][x], x, y)
 
     def add(self, nObject, x, y):
         if (0 <= x <= FIELDS_X) and (0 <= y <= FIELDS_Y):
-            self.fields[y][x] = nObject
+            self.fields[y][x].append(nObject)
+
+    def rem(self, nObject, x, y):
+        if (0 <= x <= FIELDS_X) and (0 <= y <= FIELDS_Y):
+            self.fields[y][x].remove(nObject)
 
     def checkPosition(self, x, y):
         if (0 <= x <= FIELDS_X) and (0 <= y <= FIELDS_Y):
@@ -50,8 +58,8 @@ class gamefield:
 
     def move(self, nObject, iX, iY, x, y):
         if (0 <= iX <= FIELDS_X - 1) and (0 <= iY <= FIELDS_Y - 1):
-           self.fields[y][x] = boden()
-           self.fields[iY][iX] = nObject
+           self.rem(nObject, x, y)
+           self.add(nObject, iX, iY)
 
     def handleEvent(self, event):
         for y in range(FIELDS_Y):

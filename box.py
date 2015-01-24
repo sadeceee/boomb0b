139fabc1c1c839dc235052c290f08de43a5a554a
leandro <leandrobaltazar@hotmail.de>
2015-01-24 01:45:46 +0100

    class box:     added variable 'image'     added method draw     added method load added following classes to box.py:     stone     crate     bomb

--- box.py
+++ box.py
@@ -1,6 +1,39 @@
 class box:
+    image = None
     isWall = False
     breakable = False
 
     def __init__(self):
         pass
+
+    def draw(self, screen, x, y):
+        screen.blit(self.image, (x, y))
+
+    def load(self, path):
+        pass
+
+class stone(box):
+
+    def __init__(self):
+        super(box)
+
+        self.isWall = True
+        self.breakable = False
+
+
+class crate(box):
+
+    def __init__(self):
+        super(box)
+
+        self.isWall = True
+        self.breakable = True
+
+
+class bomb(box):
+
+    def __init__(self):
+        super(box)
+
+        self.isWall = True
+        self.breakable = False

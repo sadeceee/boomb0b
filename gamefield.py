9184f9a916214dea3dc588dceeaeef2c291f2df3
Pascal Ehlke <pe@capricorn.de>
2015-01-24 04:06:52 +0100

    Added map update for random crate generation Added gamefield update for random crate generation

--- gamefield.py
+++ gamefield.py
@@ -1,18 +1,24 @@
 from constants import *
 from box import *
+import random
 
 class gamefield:
     fields = []
     c_screen = None
 
     def __init__(self, screen):
+        random.seed()
         self.fields = map_loader("MAP", "default.map")
         for y in range(FIELDS_Y):
             for x in range(FIELDS_X):
                 if(self.fields[y][x] == '1'):
                     self.fields[y][x] = stone()
                 elif(self.fields[y][x] == '2'):
-                    self.fields[y][x] = crate()
+                    iRandom = random.randint(2, 30)
+                    if (iRandom % 2 == 0):
+                        self.fields[y][x] = crate()
+                    else:
+                        self.fields[y][x] = boden()
                 else:
                     self.fields[y][x] = boden()
 

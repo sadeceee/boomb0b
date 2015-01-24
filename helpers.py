cbb661143acb53c7b3355b154fb2a88b1fe31d88
Pascal Ehlke <pe@capricorn.de>
2015-01-24 03:02:19 +0100

    Updated image_loader to load an image just one time

--- helpers.py
+++ helpers.py
@@ -2,5 +2,10 @@ import os
 import pygame
 
 
+images = {}
+
 def image_loader(dir, filename):
-    return pygame.image.load(os.path.join(dir, filename))
+    if filename not in images:
+        images[filename] = pygame.image.load(os.path.join(dir, filename))
+
+    return images[filename]
